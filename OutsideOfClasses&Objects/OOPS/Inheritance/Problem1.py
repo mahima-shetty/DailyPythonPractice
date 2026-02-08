5f0652e57d97afd3199cef4210ef9cf13b1e3d7d
Mahima Shetty <41589522+mahima-shetty@users.noreply.github.com>
2026-02-08 16:15:08 +0530

    Add single inheritance example with Animal and Dog
    Demonstrate single inheritance with Animal and Dog classes.

--- OutsideOfClasses&Objects/OOPS/Inheritance/Problem1.py
+++ OutsideOfClasses&Objects/OOPS/Inheritance/Problem1.py
@@ -1,1 +1,31 @@
 
+# ## 3. Inheritance (5 Coding Questions)
+
+# 1. Write a program demonstrating single inheritance using:
+#   - Base class `Animal`
+#   - Derived class `Dog`
+
+
+class Animal:
+    def __init__(self):
+        self.species = "Animal"
+        self.limbs = 4
+        self.eyes = 2
+    def walk(self):
+        return ("I can walk with " + str(self.limbs) +" limbs. ")
+        
+class Dog(Animal):
+    def __init__(self):
+        super().__init__()
+        self.color = "Black"
+        
+    def bark(self):
+        return "I can bark"
+
+    
+d1 = Dog()
+print(d1.walk())
+print(d1.bark())
+print(d1.limbs)
+print(d1.eyes)
+print(d1.color)
