bcf614ec3c752b565dfd07a9913ad20351592fe3
Mahima Shetty <41589522+mahima-shetty@users.noreply.github.com>
2026-02-12 15:04:39 +0530

    Add Fibonacci generator function
    Implement a generator function to yield Fibonacci numbers up to a specified limit.

---
+++ OutsideOfClasses&Objects/Generators&Decorators/Generators/ProblemsIntermediate.py
@@ -0,0 +1,24 @@
+# Level 2: Intermediate
+
+# Write a generator that yields Fibonacci numbers up to a given limit
+def gen_fibo(n):
+    a = 1
+    b = 1
+    yield a
+    yield b
+    n -= 2 
+    while n != 0 :
+        c = a + b
+        yield c
+        a = b
+        b = c
+        n -= 1
+
+
+gen1 = gen_fibo(7)
+
+while True:
+    try:
+        print(next(gen1))
+    except StopIteration:
+        pass
