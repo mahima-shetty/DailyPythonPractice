b8fabc62fd2c74368f768a7b3da0b680b7e77f0b
Mahima Shetty <41589522+mahima-shetty@users.noreply.github.com>
2026-03-12 23:52:49 +0530

    Update GeneratorsAdvanced.py

--- OutsideOfClasses&Objects/Generators&Decorators/Generators/GeneratorsAdvanced.py
+++ OutsideOfClasses&Objects/Generators&Decorators/Generators/GeneratorsAdvanced.py
@@ -13,3 +13,20 @@ i = inf_2()
 print(next(i))
 print(next(i))
 print(next(i))
+
+
+
+# Implement a generator that mimics the behavior of range(start, stop, step).
+
+def range_t(start, stop, step):
+    current = start
+    while current <= stop:
+        yield current
+        current += step
+        
+        
+r = range_t(3,5,1)
+
+print(next(r))
+print(next(r))
+print(next(r))
